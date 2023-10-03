11efe75d5fb374ccfb809ca1c848947fc396c514
facuramosg <facundoramosgarde@gmail.com>
2023-10-03 17:07:29 -0300

    fix all img

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,8 +13,31 @@ module.exports = {
 				'bounce-slow': 'bounce 3s linear infinite',
 			  }
 		},
+		patterns: {
+			opacities: {
+				100: "1",
+				80: ".80",
+				60: ".60",
+				40: ".40",
+				20: ".20",
+				10: ".10",
+				5: ".05",
+			},
+			sizes: {
+				1: "0.25rem",
+				2: "0.5rem",
+				4: "1rem",
+				6: "1.5rem",
+				8: "2rem",
+				16: "4rem",
+				20: "5rem",
+				24: "6rem",
+				32: "8rem",
+			}
+		},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		require('flowbite/plugin'),
+		require('tailwindcss-bg-patterns')
 	],
 }
