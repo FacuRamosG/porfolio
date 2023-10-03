67edf9798e1b6cc7c7144a6572aa30155a0379ef
facuramosg <facundoramosgarde@gmail.com>
2023-10-03 15:38:54 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		fontFamily:{
+			sans:['Gotham SSm A , sans-serif']
+		},
+		extend: {
+			animation: {
+				'bounce-slow': 'bounce 3s linear infinite',
+			  }
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
